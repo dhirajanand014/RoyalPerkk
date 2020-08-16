3d752f460f808f097dce32d8d83d2f21a35d5ac9
dhirajanand <dhiraj.na@gmail.com>
2020-08-16 16:45:42 +0530

    First commit

---
+++ app/src/main/java/aegismatrix/com/royalperkk/RPSplashScreen.java
@@ -0,0 +1,83 @@
+package aegismatrix.com.royalperkk;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+/**
+ * Main class for splash screen.
+ */
+public class RPSplashScreen extends AppCompatActivity implements Observer {
+    private NetworkChangeReceiver networkChangeReceiverSplashScreen = new NetworkChangeReceiver();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        ObservableObject.getInstance().addObserver(this);
+        registerReceiver(networkChangeReceiverSplashScreen, intentFilter);
+    }
+
+    /**
+     * Transition from Left to Right
+     *
+     * @param context
+     */
+    public void animateSlideLeft(Context context) {
+        ((RPSplashScreen) context).overridePendingTransition(R.anim.animate_slide_left_enter, R.anim.animate_slide_left_exit);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ObservableObject.getInstance().deleteObserver(this);
+        unregisterReceiver(networkChangeReceiverSplashScreen);
+    }
+
+    @Override
+    public void update(Observable observable, Object intent) {
+        if (null != intent) {
+            boolean status = ((Intent) intent).getBooleanExtra("status", Boolean.TRUE);
+            Snackbar snackBar = Snackbar.make(findViewById(android.R.id.content), status ? "Connected to internet." : "Check internet connection!!", status ? Snackbar.LENGTH_SHORT : Snackbar.LENGTH_INDEFINITE);
+            // get snackbar view
+            View mView = snackBar.getView();
+            Snackbar.SnackbarLayout layout = (Snackbar.SnackbarLayout) mView;
+
+            layout.setPadding(0, 0, 0, 0);//set padding to 0
+            // get textview inside snackbar view
+            TextView mTextView = mView.findViewById(com.google.android.material.R.id.snackbar_text);
+            // set text to center
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                mTextView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            } else {
+                mTextView.setGravity(Gravity.CENTER_HORIZONTAL);
+            }
+            mTextView.setBackgroundColor(ContextCompat.getColor(RPSplashScreen.this, status ? R.color.colorGreen : R.color.design_default_color_error));
+            // show the snackbar
+            snackBar.show();
+
+            if (status) {
+                Intent mainIntent = new Intent(RPSplashScreen.this, MainActivity.class);
+                startActivity(mainIntent);
+                animateSlideLeft(this);
+                finish();
+            }
+        }
+    }
+}
